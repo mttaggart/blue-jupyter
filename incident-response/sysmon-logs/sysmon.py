3f20b543f7245e1c10cf3f3335bc02d1443c1a99
Michael Taggart <michael.t.taggart@gmail.com>
2021-09-22 11:20:44 -0700

    All Relevant IDs Classed

--- incident-response/sysmon-logs/sysmon.py
+++ incident-response/sysmon-logs/sysmon.py
@@ -17,12 +17,16 @@ class SysmonEvent:
         self.soup = soup
         self.user_id = soup.security.attrs["userid"]
         self.event_id = soup.eventid.text
+        
         self.time_created = soup.timecreated.attrs["systemtime"]
         self.pid = SysmonEvent.extract_event_data(soup, "data[name='ProcessId']")
         self.image = SysmonEvent.extract_event_data(soup, "data[name='Image']")
         self.user = SysmonEvent.extract_event_data(soup, "data[name='User']")
 
 class ProcessCreate(SysmonEvent):
+    """
+    Event ID 1
+    """
     def __init__(self, soup):
         super().__init__(soup)
         self.command_line = SysmonEvent.extract_event_data(soup, "data[name='CommandLine']")
@@ -30,7 +34,20 @@ class ProcessCreate(SysmonEvent):
         self.parent_image = SysmonEvent.extract_event_data(soup, "data[name='ParentImage']")
         self.integrity_levels = SysmonEvent.extract_event_data(soup, "data[name='IntegrityLevel']")
 
+class FileCreationTimeChanged(SysmonEvent):
+    """
+    Event ID 2
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.target_filename = SysmonEvent.extract_event_data(soup, "data[name='TargetFilename']")
+        self.creation_utc_time = SysmonEvent.extract_event_data(soup, "data[name='CreationUtcTime']")
+        self.previous_creation_utc_time = SysmonEvent.extract_event_data(soup, "data[name='PreviousCreationUtcTime']")        
+
 class NetworkConnect(SysmonEvent):
+    """
+    Event ID 3
+    """
     def __init__(self, soup):
         super().__init__(soup)
         self.src_ip = SysmonEvent.extract_event_data(soup, "data[name='SourceIp']")
@@ -39,10 +56,137 @@ class NetworkConnect(SysmonEvent):
         self.dest_port = SysmonEvent.extract_event_data(soup, "data[name='DestinationPort']")
         self.integrity_levels = SysmonEvent.extract_event_data(soup, "data[name='IntegrityLevel']")
 
+class DriverLoaded(SysmonEvent):
+    """
+    Event ID 6
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.image_loaded = SysmonEvent.extract_event_data(soup, "data[name='ImageLoaded']")
+        self.signed = SysmonEvent.extract_event_data(soup, "data[name='Signed']")
+        self.hashes = SysmonEvent.extract_event_data(soup, "data[name='Hashes']")
+        self.signature = SysmonEvent.extract_event_data(soup, "data[name='Signature']")
+        self.signature_status = SysmonEvent.extract_event_data(soup, "data[name='SignatureStatus']")
+
+class ImageLoaded(SysmonEvent):
+    """
+    Event ID 7
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.image_loaded = SysmonEvent.extract_event_data(soup, "data[name='ImageLoaded']")
+        self.signed = SysmonEvent.extract_event_data(soup, "data[name='Signed']")
+        self.hashes = SysmonEvent.extract_event_data(soup, "data[name='Hashes']")
+        self.signature = SysmonEvent.extract_event_data(soup, "data[name='Signature']")
+        self.signature_status = SysmonEvent.extract_event_data(soup, "data[name='SignatureStatus']")
+
+class CreateRemoteThread(SysmonEvent):
+    """
+    Event ID 8
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.source_pid = SysmonEvent.extract_event_data(soup, "data[name='SourceProcessId']")
+        self.target_pid = SysmonEvent.extract_event_data(soup, "data[name='TargetProcessId']")
+        self.source_image = SysmonEvent.extract_event_data(soup, "data[name='SourceImage']")
+        self.target_image = SysmonEvent.extract_event_data(soup, "data[name='TargetImage']")
+        self.new_thread_id = SysmonEvent.extract_event_data(soup, "data[name='NewThreadId']")
+
+class RawAccessRead(SysmonEvent):
+    """
+    Event ID 9
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.device = SysmonEvent.extract_event_data(soup, "data[name='Device']")
+
+class FileCreate(SysmonEvent):
+    """
+    Event ID 11
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.target_filename = SysmonEvent.extract_event_data(soup, "data[name='TargetFilename']")
+
+class RegistryCreateDelete(SysmonEvent):
+    """
+    Event ID 12
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.target_object = SysmonEvent.extract_event_data(soup, "data[name='TargetObject']")
+
+class RegistryValueSet(SysmonEvent):
+    """
+    Event ID 13
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.target_object = SysmonEvent.extract_event_data(soup, "data[name='TargetObject']")
+        self.details = SysmonEvent.extract_event_data(soup, "data[name='Details']")
+
+class RegistryKeyValueRename(SysmonEvent):
+    """
+    Event ID 14
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.target_object = SysmonEvent.extract_event_data(soup, "data[name='TargetObject']")
+        self.new_name = SysmonEvent.extract_event_data(soup, "data[name='NewName']")
+
+class PipeEvent(SysmonEvent):
+    """
+    Event ID 17, 18
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.pipe_name = SysmonEvent.extract_event_data(soup, "data[name='PipeName']")
+
+class WmiEventFilter(SysmonEvent):
+    """
+    Event ID 19
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.event_namespace = SysmonEvent.extract_event_data(soup, "data[name='EventNamespace']")
+        self.filter_name = SysmonEvent.extract_event_data(soup, "data[name='Name']")
+        self.query = SysmonEvent.extract_event_data(soup, "data[name='Query']")
+
+class DNSEvent(SysmonEvent):
+    """
+    Event ID 22
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.query_name = SysmonEvent.extract_event_data(soup, "data[name='QueryName']")
+        self.query_results = SysmonEvent.extract_event_data(soup, "data[name='QueryResults']")
+
+class ProcessTampering(SysmonEvent):
+    """
+    Event ID 25
+    """
+    def __init__(self, soup):
+        super().__init__(soup)
+        self.type = SysmonEvent.extract_event_data(soup, "data[name='Type']")
+
 
 EVENT_TYPES = {
     "1": ProcessCreate,
-    "3": NetworkConnect
+    "2": FileCreationTimeChanged,
+    "3": NetworkConnect,
+    "6": DriverLoaded,
+    "7": ImageLoaded,
+    "8": CreateRemoteThread,
+    "9": RawAccessRead,
+    "11": FileCreate,
+    "12": RegistryCreateDelete,
+    "13": RegistryValueSet,
+    "14": RegistryKeyValueRename,
+    "17": PipeEvent,
+    "18": PipeEvent,
+    "19": WmiEventFilter,
+    "22": DNSEvent,
+    "25": ProcessTampering
 }
 
 def load_event_xml(file_path):
