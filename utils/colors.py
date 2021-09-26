df83210453e1ba5a52c26e3e60c918743411af5a
husky <huskyhacks.mk@gmail.com>
2021-09-25 23:11:36 -0400

    refactor all code to be object-oriented

--- utils/colors.py
+++ utils/colors.py
@@ -8,9 +8,6 @@ important = (Fore.CYAN + "[!]" + Fore.RESET + " ")
 printError = (Fore.RED + "[X]" + Fore.RESET + " ")
 
 # Confidence interval colors
-"""
-None: 0-2 | Low: 3-4 | Med: 5-6 | High: 7-8 | Crit: 9-10
-"""
 none = (Fore.GREEN + "[*]" + Fore.RESET + " ")
 low = (Fore.CYAN + "[*]" + Fore.RESET + " ")
 med = (Fore.YELLOW + "[*]" + Fore.RESET + " ")
